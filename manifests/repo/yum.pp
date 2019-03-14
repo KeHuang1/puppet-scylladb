1e6c8cd6cdad6e0134f02461b9a35e9cc596a817
KeHuang1 <kehua@keystep.no>
2019-03-14 15:20:22 +0100

    change scylla yum.repo

--- manifests/repo/yum.pp
+++ manifests/repo/yum.pp
@@ -4,30 +4,28 @@
 #
 class scylla::repo::yum {
   yumrepo { 'scylla':
-    baseurl  =>  "https://s3.amazonaws.com/downloads.scylladb.com/rpm/centos/scylladb-${scylla::major_version}/\$releasever/\$basearch/",
+    baseurl  =>  "https://repositories.scylladb.com/scylla/downloads/scylladb/e46d05ba-d2cc-4594-8387-5986145a4eea/scylla/rpm/centos/scylladb-${scylla::major_version}/\$releasever/\$basearch/",
     descr    =>  'Scylla for Centos $releasever - $basearch',
     enabled  =>  1,
     gpgcheck =>  0,
   }
 
   yumrepo { 'scylla-generic':
-    baseurl  =>  "https://s3.amazonaws.com/downloads.scylladb.com/rpm/centos/scylladb-${scylla::major_version}/\$releasever/noarch/",
+    baseurl  =>  "=https://repositories.scylladb.com/scylla/downloads/scylladb/e46d05ba-d2cc-4594-8387-5986145a4eea/scylla/rpm/centos/scylladb-${scylla::major_version}/\$releasever/noarch/",
     descr    =>  'Scylla for centos $releasever',
     enabled  =>  1,
     gpgcheck =>  0,
   }
 
   yumrepo { 'scylla-3rdparty':
-    baseurl  =>  "https://s3.amazonaws.com/downloads.scylladb.com/rpm/centos/scylladb-${scylla::major_version}/\$releasever/\$basearch/",
-    descr    =>  'Scylla 3rdParty for Centos $releasever - $basearch',
+    baseurl  =>  "https://copr-be.cloud.fedoraproject.org/results/scylladb/scylla-3rdparty/epel-7-x86_64/",
+    descr    =>  'Copr repo for scylla-3rdparty owned by scylladb',
+    skip_if_unavailable => 'True',
+    gpgcheck =>  1,
     enabled  =>  1,
-    gpgcheck =>  0,
+    gpgkey  =>  'https://copr-be.cloud.fedoraproject.org/results/scylladb/scylla-3rdparty/pubkey.gpg',
+    repo_gpgcheck => 0,
+
   }
 
-  yumrepo { 'scylla-3rdparty-generic':
-    baseurl  =>  "https://s3.amazonaws.com/downloads.scylladb.com/rpm/centos/scylladb-${scylla::major_version}/\$releasever/noarch/",
-    descr    =>  'Scylla 3rdParty for Centos $releasever',
-    enabled  =>  1,
-    gpgcheck =>  0,
-  }
 }
